5f224472f3862966a1dee6bb1199fd68f54c61e1
Joshua Yurkovich <jyurkovi@kent.edu>
2016-10-20 19:47:35 -0400

    Partial Line Following Code

---
+++ line following program.h
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using std::cout; using std::cin; using std::endl;
+
+const int size = 5;
+const float k = .25;
+
+int main() {
+	float sensors [size];
+		//get input for the sensors using a loop. i dont know how to read pins :/
+		
+		double weight [size];
+		
+		int i = 0;
+		for ( int i = 0; i <size; i++)
+		{
+			int pos = o;
+			sensors [i] * weight [i] = pos;
+			pos / 4 = pos; // need to find range , maybe not 4
+		}
+		
+		double velocity
+		//input velocity from somewhere
+		
+		double result;
+		
+		k * (x * x * x) + velocity = result;  //r wheel speed
+	
+	
+	
+	
+}
